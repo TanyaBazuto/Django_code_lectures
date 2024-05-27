d25117fb91347c3a820c0e5580093c781c29dc9e
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-05-27 21:44:18 +0300

    Update views.py

--- orm_advanced/demo/views.py
+++ orm_advanced/demo/views.py
@@ -4,6 +4,6 @@ from demo.models import Order
 
 
 def list_orders(request):
-    orders = Order.objects.filter(positions__product__price__lte=600)
-    context = {'orders': orders}
+    orders = Order.objects.filter(positions__product__price__lte=600)    #формируем список заказов. Если все заказы, то orders = Order.objects.all()
+    context = {'orders': orders}                      # в context передаем все заказы из БД 
     return render(request, 'orders.html', context)    #возвращаем шаблон - генерируем с помощью шаблона новый html-код
