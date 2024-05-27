e39b9a446f93b033ace1dbc70bacb2350a43c60b
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-05-27 21:40:35 +0300

    Update views.py

--- orm_advanced/demo/views.py
+++ orm_advanced/demo/views.py
@@ -6,4 +6,4 @@ from demo.models import Order
 def list_orders(request):
     orders = Order.objects.filter(positions__product__price__lte=600)
     context = {'orders': orders}
-    return render(request, 'orders.html', context)
+    return render(request, 'orders.html', context)    #возвращаем шаблон - генерируем с помощью шаблона новый html-код
