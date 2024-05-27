75d618ccd76fb099aefb742fb7089244ffe3f5d5
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-05-27 22:00:45 +0300

    Update views.py

--- orm_advanced/demo/views.py
+++ orm_advanced/demo/views.py
@@ -7,3 +7,5 @@ def list_orders(request):
     orders = Order.objects.filter(positions__product__price__lte=600)    #формируем список заказов. Если все заказы, то orders = Order.objects.all()
     context = {'orders': orders}                      # в context передаем все заказы из БД 
     return render(request, 'orders.html', context)    #возвращаем шаблон - генерируем с помощью шаблона новый html-код
+
+#(positions__product__price__lte=600) условия для фильтрации отображаемых заказов. Проваливаемся, через модификатор ___, из positions в product, далее в его цену. Модификатор lte означает меньше или равно (см.презентацию 7.3)
