6a8d6e0d217af92ec02d437c4ffb4f11adbe7ef2
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-05-27 21:15:27 +0300

    Update models.py

--- orm_advanced/demo/models.py
+++ orm_advanced/demo/models.py
@@ -8,10 +8,12 @@ class Product(models.Model):
 
 
 class Order(models.Model):
+    # products = models.ManyToManyField(Product, related_name='orders')  #установление связи многие-ко-иногим. related_name - чтоб можно было обратиться к продуктам из заказа
     pass
 
 
+#ПРОМЕЖУТОЧНАЯ ТАБЛИЦА/МОДЕЛЬ в БД (в данном случае - для утсановления количества продуктов в заказе), которая будет отвечать за одну позицию в заказе, Т.Е. связывает Продукт и Заказ
 class OrderPosition(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='positions')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='positions') #related_name - для получения позиций в которых он участвует    
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='positions')
-    quantity = models.IntegerField()
+    quantity = models.IntegerField()    #производное поле. не связанное
