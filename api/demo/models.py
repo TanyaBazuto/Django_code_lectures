f6f7517323e3492e7d2183eddac0ad3171f52cb9
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-03 14:04:41 +0300

    Update models.py

--- api/demo/models.py
+++ api/demo/models.py
@@ -5,3 +5,5 @@ class Weapon(models.Model):
     power = models.IntegerField()
     rarity = models.CharField(max_length=50)
     value = models.IntegerField()
+
+# после создания модели: создаем миграцию (makemigrations) и применяем её (migrate)
