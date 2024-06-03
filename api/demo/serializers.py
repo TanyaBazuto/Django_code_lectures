ab1445d594a2fb25a3151b6beb8e65cb2f751db8
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-03 14:24:39 +0300

    Update serializers.py

--- api/demo/serializers.py
+++ api/demo/serializers.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 from demo.models import Weapon
 
 
-# class WeaponSerializer(serializers.Serializer):
+# class WeaponSerializer(serializers.Serializer):  # создаем сериализатор без импорта модели Weapon
 #     power = serializers.IntegerField()
 #     rarity = serializers.CharField()
 
 
-class WeaponSerializer(serializers.ModelSerializer):
-    class Meta:
+class WeaponSerializer(serializers.ModelSerializer):   # создаем сериализатор для модели Weapon
+    class Meta:               # описываем свойства, которые д/б сконвертированы в json, и наоброт из json в сложный объект
         model = Weapon
         fields = ['id', 'power', 'rarity']
