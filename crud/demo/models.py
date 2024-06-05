0cea4c558aa9207548d60bc657a3c94abe6f29dc
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-05 17:49:24 +0300

    Update models.py

--- crud/demo/models.py
+++ crud/demo/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
+# User - стандартная модель джанго, как и Group. Не забывать импортировать
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)     # on_delete - при удалении пользователя, все его комментарии удаляются
+    text = models.TextField()                                    #  TextField - не имеет ограничений по длине
     created_at = models.DateTimeField(auto_now_add=True)
+
+# после этого создаем первого пользователя - createsuperuser. И через 'python manage.py shell' создаем объекты модели(таблицы)  и незабыть сохранить - объект.save()
