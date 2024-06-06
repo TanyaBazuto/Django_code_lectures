2cad1288d33f41a89acba146fe1544c5e1583167
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-06 13:37:04 +0300

    Update settings.py

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ### Вариант настройки фильтрации
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': [                                 # DEFAULT_FILTER_BACKENDS -- список, который содержит все доступные фильтры. В DRF по умолчанию реализованы три типа фильтров
+    'DEFAULT_FILTER_BACKENDS': [                                 # DEFAULT_FILTER_BACKENDS -- список, который содержит все доступные фильтры. В DRF по умолчанию реализованы три типа фильтров: DjangoFilterBackend, SearchFilter, OrderingFilter
         'django_filters.rest_framework.DjangoFilterBackend',     # 1ый стандартная фильтрация по параметрам. Все фильтры будут использоваться для всех ViewSet из views.py/
     ],
     'SEARCH_PARAM': 'q',
