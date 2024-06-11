e2d3c98e519cf04ebbb42d3215f4e7cd2fd87dba
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-11 07:09:37 +0300

    Update serializers.py

--- auth/demo/serializers.py
+++ auth/demo/serializers.py
@@ -7,4 +7,4 @@ class AdvSerializer(serializers.ModelSerializer):
     class Meta:
         model = Adv
         fields = ['id', 'user', 'text', 'created_at', 'open']   # поля, которые необходимо отоброжать у json
-        read_only_fields = ['user',]                            # 
+        read_only_fields = ['user',]                            # указываем, что поле user только для чтения, в запросах DRF берет его по токену и нам не нужно его передавать
