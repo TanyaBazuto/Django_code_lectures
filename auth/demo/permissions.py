00b6e5e237f886f0b33aa06278b937cf25044966
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-06-14 06:59:49 +0300

    Update permissions.py

--- auth/demo/permissions.py
+++ auth/demo/permissions.py
@@ -2,7 +2,10 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):   # метод проверяет права на конкретный объект
         if request.method == 'GET':
             return True
-        return request.user == obj.user
+        return request.user == obj.user                    # совпадает ли пользователь, пославший запрос на работу с ресурсом, и пользователь создавший этот ресурс
+
+    ### метод, который означет
+    # def has_permission -- метод проверки имеет ли пользователь на работу с ресурсом в целом, т.е. создание или просмотр всего ресурса. По умолчанию возвращает True
