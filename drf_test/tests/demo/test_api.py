5401bebf3bc04ed921febc237f5d02c8aefd2d26
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-07-02 18:23:48 +0300

    Update test_api.py

--- drf_test/tests/demo/test_api.py
+++ drf_test/tests/demo/test_api.py
@@ -6,12 +6,12 @@ from model_bakery import baker
 from demo.models import Message
 
 
-@pytest.fixture
-def client():
+@pytest.fixture         #чтобы избавиться от дублирования кода в тестах - ФИКСТУРЫ -- специальные ф-ции, к-е возвращают некоторое значение и мы их можем принимать в качестве аргументов в нашу тестирующую ф-цию
+def client():           #функция, возвращающая клиента
     return APIClient()
 
 
-@pytest.fixture
+@pytest.fixture         #декоратор, помечающий, что это фикстура, и теперь ее можно указывать в качестве входного аргумента в любой из нашх тестирующх функций
 def user():
     return User.objects.create_user('admin')
 
@@ -25,7 +25,7 @@ def message_factory():
 
 ### СТАНДРАТНАЯ СТРУКТУРА ТЕСТА, СОСТОЯЩАЯ ИЗ ТРЁХ СЕКЦИЙ
 @pytest.mark.django_db                                  #декоратор, указывающий, что наш тест будет использовать БД
-def test_get_messages(client, user, message_factory):
+def test_get_messages(client, user, message_factory):   #в качестве аргументов указываем ранее написанные фикстуры
     # Arrange - 1ая секция теста -- подготовка данных: достать и положить определенные записи БД
     messages = message_factory(_quantity=10)
 
@@ -41,10 +41,11 @@ def test_get_messages(client, user, message_factory):
 
 
 @pytest.mark.django_db
-def test_create_message(client, user):
+def test_create_message(client, user):      #тест на создание сообщений 
     count = Message.objects.count()
 
-    response = client.post('/messages/', data={'user': user.id, 'text': 'test text'})
+    response = client.post('/messages/', data={'user': user.id, 'text': 'test text'})   #напишем действие по созданию message. Для передачи в json, а не в тексте, можно здесь же +аргумент format='json'
+                                                                                          # или создать настройки в settings.py  
 
-    assert response.status_code == 201
+    assert response.status_code == 201       #проверка что наш response вернулся с правильным кодом
     assert Message.objects.count() == count + 1
