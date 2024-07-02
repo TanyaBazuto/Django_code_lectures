2a59379ec585a4fe0c926b1f7b7cfc917479004d
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-07-02 19:08:48 +0300

    Update test_api.py

--- drf_test/tests/demo/test_api.py
+++ drf_test/tests/demo/test_api.py
@@ -1,11 +1,11 @@
 import pytest
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient         #получение клиента, который сможет отправлять запросы в наш api-сервис
-from model_bakery import baker
+from model_bakery import baker                    #pip install model_bakery - библиотека для создания рандомных данных            
 
 from demo.models import Message
 
-
+### СОЗДАНИЕ ФИКСТУР
 @pytest.fixture         #чтобы избавиться от дублирования кода в тестах - ФИКСТУРЫ -- специальные ф-ции, к-е возвращают некоторое значение и мы их можем принимать в качестве аргументов в нашу тестирующую ф-цию
 def client():           #функция, возвращающая клиента
     return APIClient()
@@ -15,10 +15,10 @@ def client():           #функция, возвращающая клиента
 def user():
     return User.objects.create_user('admin')
 
-
+### ФИКСТУРА ДЛЯ СОЗДАНИЯ ФАБРИКИ ОБЪЕКТОВ С ПОМОЩЬЮ библиотеки model_bakery
 @pytest.fixture
-def message_factory():
-    def factory(*args, **kwargs):
+def message_factory():             #создания рандомных данных, Н: большого количества сообщений, чтобы проверить как ведет себя пагинация, фильтрация и пр.функции сервера
+    def factory(*args, **kwargs):       #вместо создания по одному сообщению Message.objects.create(user=user.id, text='test')
         return baker.make(Message, *args, **kwargs)
 
     return factory
@@ -27,7 +27,7 @@ def message_factory():
 @pytest.mark.django_db                                  #декоратор, указывающий, что наш тест будет использовать БД
 def test_get_messages(client, user, message_factory):   #в качестве аргументов указываем ранее написанные фикстуры
     # Arrange - 1ая секция теста -- подготовка данных: достать и положить определенные записи БД
-    messages = message_factory(_quantity=10)
+    messages = message_factory(_quantity=10)            #_quantity - сколько объектов нужно создать            
 
     # Act - 2ая секция теста -- непосредственно тестируемый функционал: например вызов того или иного метода
     response = client.get('/messages/')
@@ -42,10 +42,10 @@ def test_get_messages(client, user, message_factory):   #в качестве а�
 
 @pytest.mark.django_db
 def test_create_message(client, user):      #тест на создание сообщений 
-    count = Message.objects.count()
+    count = Message.objects.count()         #запрашиваем количество сообщений в БД
 
     response = client.post('/messages/', data={'user': user.id, 'text': 'test text'})   #напишем действие по созданию message. Для передачи в json, а не в тексте, можно здесь же +аргумент format='json'
                                                                                           # или создать настройки в settings.py  в секции REST_FRAMEWORK
 
     assert response.status_code == 201       #проверка что наш response вернулся с правильным кодом
-    assert Message.objects.count() == count + 1
+    assert Message.objects.count() == count + 1   #проверяем что количество сообщений после создания (post) == изначальное количество +1
