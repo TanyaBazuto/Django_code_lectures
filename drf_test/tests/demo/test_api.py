dd302bfe7561c8faa5c08666ce125cb467c4fea0
TanyaBazuto <150078962+TanyaBazuto@users.noreply.github.com>
2024-07-02 17:39:19 +0300

    Update test_api.py

--- drf_test/tests/demo/test_api.py
+++ drf_test/tests/demo/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 from django.contrib.auth.models import User
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient         #получение клиента, который сможет отправлять запросы в наш api-сервис
 from model_bakery import baker
 
 from demo.models import Message
@@ -23,21 +23,20 @@ def message_factory():
 
     return factory
 
-
-
-@pytest.mark.django_db
+### СТАНДРАТНАЯ СТРУКТУРА ТЕСТА, СОСТОЯЩАЯ ИЗ ТРЁХ СЕКЦИЙ
+@pytest.mark.django_db                                  #декоратор, указывающий, что наш тест будет использовать БД
 def test_get_messages(client, user, message_factory):
-    # Arrange
+    # Arrange - 1ая секция теста -- подготовка данных: достать и положить определенные записи БД
     messages = message_factory(_quantity=10)
 
-    # Act
+    # Act - 2ая секция теста -- непосредственно тестируемый функционал: например вызов того или иного метода
     response = client.get('/messages/')
 
-    # Assert
-    assert response.status_code == 200
-    data = response.json()
-    assert len(data) == len(messages)
-    for i, m in enumerate(data):
+    # Assert - 2ая секция теста -- проверка, того что действие действительно выполнено корректно
+    assert response.status_code == 200     #проверка статуса возврата ответа от api
+    data = response.json()                 #получение содержимого ответа в json для его просмотра и проверки (например, что ответ с правильным статусом не пустой
+    assert len(data) == len(messages)      #проверка что длина полученного списка значений равна списку записей в БД
+    for i, m in enumerate(data):           #проверка содержимого текста messages -- обращение к данным полученным в ответе в json-файле, которые содержаться в форме словаря
         assert m['text'] == messages[i].text
 
 
